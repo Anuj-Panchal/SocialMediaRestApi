54ce45f217c786ff9210469992f0a532d99db0fc
Anuj Panchal <anuj.panchal@moglix.com>
2024-04-08 11:50:47 +0530

    add cors mapping and Dockerfile

--- src/main/java/com/anuj/socialMediaApp/SocialMediaAppApplication.java
+++ src/main/java/com/anuj/socialMediaApp/SocialMediaAppApplication.java
@@ -2,12 +2,29 @@ package com.anuj.socialMediaApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SocialMediaAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaAppApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer getWebConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
--- src/main/java/com/anuj/socialMediaApp/controller/UserController.java
+++ src/main/java/com/anuj/socialMediaApp/controller/UserController.java
@@ -5,10 +5,17 @@ import com.anuj.socialMediaApp.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 public class UserController {
 
